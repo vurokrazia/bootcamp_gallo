eae9d50552c9af481fbd8fcf1f761518ce27920c
Jesus <jesus.alberto.vk@gmail.com>
2022-04-14 22:43:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var nop = "Jesus"
+var empty;
+/*
+    entero - int
+    double - decimal
+    boolean - boolean
+*/
+var condicion = false
+empty = "No esta vacio"
+console.log(empty);
+
+var edad = Math.random() * 20
+console.log(edad)
+// Estructura de control
+if(edad >= 18) { // Si (Se cumple esta condicion)
+    console.log("Mayor de edad");
+} else { // Si no se cumple has esto
+    console.log("Menor de edad")
+}
+
+// Bucles
+var asdasdas = []
+var fruits = []
+var vegetals = []
+
+while(condicion) { // Mientras (la condicion se cumpla)
+    console.log("hola")
+}
+
+for (var index = 0; index < 100; index++) {
+    fruits.push(index);
+}
+
+console.log(fruits.length)
